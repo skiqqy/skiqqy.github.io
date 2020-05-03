c3be9a76d1bd3f593f5a989bd265eb147e5633a9
Stephen Cochrane <skippycochrane@gmail.com>
2020-05-03 17:26:21 +0200

    Make file now generates unique blogs, just need to deal with parsing now

--- Makefile
+++ Makefile
@@ -7,13 +7,29 @@ EXE=bin/GenBlog
 all: install
 	$(CC) $(FLAGS) src/GenBlog.c -o $(EXE)
 
+debug: install
+	$(CC) $(FLAGS) -DDEBUG src/GenBlog.c -o $(EXE)
+
 install:
 	mkdir -p bin
 	mkdir -p obj
+	mkdir -p blogs
 
 clean:
 	rm -rf obj
 	rm -rf bin
 
 blog: all
+	@# Reset all blogs to start build.
+	-rm -rf  blogs/*.html
+	@# Setup boilerplate html files for blog files
+	-@for blog in ./blogs/RawBlogs/*.blog ; do \
+		blog=$$(echo $$blog | cut -c 17- | cut -d "." -f 1); \
+		cp ./blogs/preamble/start.html ./blogs/$$blog.html; \
+		done
 	./bin/GenBlog
+	@# Append the end of the file once done
+	-@for blog in ./blogs/RawBlogs/*.blog ; do \
+		blog=$$(echo $$blog | cut -c 17- | cut -d "." -f 1); \
+		cat ./blogs/preamble/end.html >> ./blogs/$$blog.html; \
+		done
--- src/GenBlog.c
+++ src/GenBlog.c
@@ -1,6 +1,76 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<dirent.h>
+#include<string.h>
+
+void parseBox(void);
+void parseNoBox(void);
+void genBP(char *file);
+void echoFile(FILE *fp);
+int ch;
 
 int main() {
+	DIR *dir;
+	struct dirent *ent;
+	char buff[256];
+	
+	if ((dir = opendir("./blogs/RawBlogs")) != NULL) {
+		while ((ent = readdir(dir)) != NULL) {
+			if (strcmp(ent->d_name, ".") && strcmp(ent->d_name, "..")) {
+				printf("file = %s\n", ent->d_name);
+				sprintf(buff, "./blogs/RawBlogs/");
+				strcat(buff, ent->d_name);
+				printf("opening, %s\n", buff);
+				genBP(buff);
+			}
+		}
+	}
 	return EXIT_SUCCESS;
+}
+
+void genBP(char *file) {
+	FILE *fp = fopen(file, "r");
+	char buff[256];
+	echoFile(fopen(file, "r"));
+
+	//Begin parsing the file.
+	ch = getc(fp);
+	while (ch != EOF) {
+		switch (ch) {
+			case '-':
+				ch = getc(fp);
+				parseBox();
+				break;
+			case '[':
+				ch = getc(fp);
+				parseNoBox();
+				break;
+		}
+		ch = getc(fp);
+	}
+}
+
+void echoFile(FILE *fp) {
+	char line[256] = "";
+	ch = getc(fp);
+
+	printf("Echo file\n");
+	while (ch != EOF) {
+		if (ch != '\n') {
+			strcat(line, &ch);
+		} else {
+			printf("%s\n", line);
+			sprintf(line, "");
+		}
+		ch = getc(fp);
+	}
+	fclose(fp);
+}
+
+void parseBox() {
+
+}
+
+void parseNoBox() {
+
 }
