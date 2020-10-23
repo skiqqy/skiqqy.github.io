04dcf9eedc6a114365aa4f87b4e88c0a68f72e08
Stephen Cochrane <skippycochrane@gmail.com>
2020-10-24 01:08:10 +0200

    Added a script to generate the website

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Auther: Skiqqy
+
+HOST="https://github.com/"
+
+conv () {
+	# Take a file and convert it to html by making substitutes.
+
+	# ^/name -> https://github.com/name
+	# */name.domain -> https://name.domain
+	sed -E "s|(\\^/)([^ \)]*)|^\/<a href=$HOST/\2>\2</a>|g" |
+	sed -E "s|(\\*/)([^ \)]*)|*\/<a href=https:\/\/\2/>\2</a>|g" |
+
+	sed -E '/%%BODY%%/r /dev/stdin' raw/template.html |
+	sed -E '/%%BODY%%/d'
+}
+
+for file in raw/*.txt
+do
+	case $file in
+		*index.txt)
+			path=index.html
+			;;
+		*)
+			path=${file/raw/site}
+			path=${path/txt/html}
+			;;
+	esac
+
+	echo "Building $file"
+	conv < "$file" > $path
+done
