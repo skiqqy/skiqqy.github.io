26e58471c5bd773720ce8d96c0ddf309984be9b9
Stephen Cochrane <skippycochrane@gmail.com>
2021-03-25 09:12:02 +0200

    Improved build script

--- build.sh
+++ build.sh
@@ -12,6 +12,9 @@ conv () {
 	sed -E "s|(\\^/)([^ \)]*)|^\/<a href=$HOST/\2>\2</a>|g" |
 	sed -E "s|(\\*/)([^ \)]*)|*\/<a href=https:\/\/\2/>\2</a>|g" |
 	sed -E "s|(\\!/)([^ \)]*)|!\/<a href=http:\/\/\2/>\2</a>|g" |
+	sed -E 's|^SBLOCK (.+)\.|<div class=box><div class=boxheader>\1</div><br>|g' |
+	sed -E 's|^EBLOCK|</div>|g' |
+	sed -E 's|\\\\|<br>|g' |
 
 	sed -E '/%%BODY%%/r /dev/stdin' raw/template.html |
 	sed -E '/%%BODY%%/d' |
